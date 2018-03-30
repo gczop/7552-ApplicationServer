2a43f861e44ee473ec1b94b5b47096c95f12807f
Guido Peirano <guidopeirano316@gmail.com>
2018-03-30 18:30:17 -0300

    Probando heroku db mongo

--- databases/users.py
+++ databases/users.py
@@ -1,7 +1,23 @@
+import os
 import pymongo
 from pymongo import MongoClient
-client = MongoClient()
-db = client.StoriesAppServer
+
+import pymongo
+
+MONGO_URL = os.environ.get('MONGOHQ_URL')
+
+if MONGO_URL:
+    # Get a connection
+    conn = pymongo.MongoClient(MONGO_URL)
+    
+    # Get the database
+    db = conn[urlparse(MONGO_URL).path[1:]]
+else:
+    # Not on an app with the MongoHQ add-on, do some localhost action
+    conn = pymongo.MongoClient('localhost', 27017)
+    db = conn['StoriesAppServer']
+
+
 userCollection = db.User
 
 
