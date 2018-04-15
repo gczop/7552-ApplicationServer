a42b476628c90972381578efbd2a3f45ab8987d0
rodrivaldivia <rodrivaldivia10@gmail.com>
2018-04-15 03:09:38 -0300

    Emprolijo y agrego un par de pruebas

--- tests.py
+++ tests.py
@@ -1,43 +1,26 @@
-import os
 from app import application as app
 import unittest
-import tempfile
 import json
 
-from werkzeug.contrib.fixers import ProxyFix
 
 class AppServerTestCase(unittest.TestCase):
 
-    app.wsgi_app = ProxyFix(app.wsgi_app)
-
     def setUp(self):
-        # self.db_fd, app.config['StoriesAppServer'] = tempfile.mkstemp()
-        #
-        # # flaskr.app.testing = True
-        # # self.app = flaskr.app.test_client()
-        #
-        # # creates a test client
+        # creates a test client
         self.app = app.test_client()
-        # # propagate the exceptions to the test client
+        # propagate the exceptions to the test client
         self.app.testing = True
         return
 
 
     def test_home_status_code(self):
-        # sends HTTP GET request to the application
-        # on the specified path
         result = self.app.get('/')
 
-        print (result.data)
-
-        # assert the status code of the response
+        # Assert the server is running correctly
         self.assertEqual(result.status_code, 200)
 
 
-    def test_signup_post(self):
-        # sends HTTP POST request to the application
-        # on the specified path
-        print("Reqs to sign up")
+    def test_correct_signup_post(self):
         loginInfo = {
             "user": "user",
             "password": "password",
@@ -47,18 +30,18 @@ class AppServerTestCase(unittest.TestCase):
 
         print (result)
 
-        # assert the status code of the response
+        # Assert a correct sign up
         self.assertEqual(result.status_code, 200)
 
-    def tearDown(self):
 
-        # os.close(self.db_fd)
-        #
-        # os.unlink(app.config['StoriesAppServer'])
-        return
+    def test_signup_post_without_pass_or_fbtoken(self):
+        loginInfo = { "user": "user" };
+        result = self.app.post('/api/users/signup', data=json.dumps(loginInfo), content_type='application/json')
+
+        # Assert an error from the server
+        self.assertNotEqual(result.status_code, 200)
+
+
 
 if __name__ == '__main__':
-    unittest.main()
-
-def run():
     unittest.main()
--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,4 @@
 from app import application
-from tests import AppServerTestCase as test
-
-
-if __name__ == "__main__":
-    test.run()
-
 
 if __name__ == "__main__":
     application.run()
-
-
-# TODO hacer que no corra solo el server
