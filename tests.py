ac796e569a74c008a131bbba9ee267781e0ef15a
rodrivaldivia <rodrivaldivia10@gmail.com>
2018-04-16 00:58:07 -0300

    Refactoring de tests que va a ser util mas adelante

--- tests.py
+++ tests.py
@@ -3,85 +3,15 @@ import os
 os.environ["TEST_ENV"] = "true"
 from app import application as app
 import unittest
-import json
+from test import test_users , test_server
 
+loader = unittest.TestLoader()
+suite  = unittest.TestSuite()
 
+# add tests to the test suite
+suite.addTests(loader.loadTestsFromModule(test_server))
+suite.addTests(loader.loadTestsFromModule(test_users))
 
-class AppServerTestCase(unittest.TestCase):
-
-    def setUp(self):
-        # creates a test client
-        self.app = app.test_client()
-        # propagate the exceptions to the test client
-        self.app.testing = True
-
-
-    def test_home_status_code(self):
-        result = self.app.get('/')
-
-        # Assert the server is running correctly
-        self.assertEqual(result.status_code, 200)
-
-
-    def test_correct_signup_post(self):
-        signupInfo = {
-            "username": "user",
-            "password": "password"
-        };
-        result = self.app.post('/api/users/signup', data=json.dumps(signupInfo), content_type='application/json')
-
-        print (result)
-
-        # Assert a correct sign up
-        self.assertEqual(result.status_code, 200)
-
-
-    def test_signup_post_without_pass_or_fbtoken(self):
-        signupInfo = { "username": "user2" };
-        result = self.app.post('/api/users/signup', data=json.dumps(signupInfo), content_type='application/json')
-
-        # Assert an error from the server
-        # TODO ver que error es el que deberia tirar
-        self.assertNotEqual(result.status_code, 200)
-
-    def test_signup_and_login(self):
-        signupInfo = {
-            "username": "user2",
-            "password": "password2"
-        };
-        loginInfo = {
-            "username": "user2",
-            "password": "password2"
-        };
-        result_signup = self.app.post('/api/users/signup', data=json.dumps(signupInfo), content_type='application/json')
-        self.assertEqual(result_signup.status_code, 200)
-        result_login = self.app.post('/api/users/login', data=json.dumps(loginInfo), content_type='application/json')
-        self.assertEqual(result_login.status_code, 200)
-
-    def test_login_nonexistant_user(self):
-        loginInfo = {
-            "username": "fakeuser",
-            "password": "password"
-        };
-        result_login = self.app.post('/api/users/login', data=json.dumps(loginInfo), content_type='application/json')
-        self.assertNotEqual(result_login.status_code, 200)
-
-    def test_login_already_registered_user(self):
-        loginInfo = {
-            "username": "user",
-            "password": "password"
-        };
-        result_login = self.app.post('/api/users/login', data=json.dumps(loginInfo), content_type='application/json')
-        self.assertEqual(result_login.status_code, 200)
-
-    def test_wrong_pass_login(self):
-        loginInfo = {
-            "username": "user",
-            "password": "wrong_password"
-        };
-        result_login = self.app.post('/api/users/login', data=json.dumps(loginInfo), content_type='application/json')
-        self.assertNotEqual(result_login.status_code, 200)
-
-
-if __name__ == '__main__':
-    unittest.main()
+# initialize a runner, pass it your suite and run it
+runner = unittest.TextTestRunner(verbosity=3)
+result = runner.run(suite)
