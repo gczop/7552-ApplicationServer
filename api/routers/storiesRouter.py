1fc951ab0af6f1902cc21b5100b68e9964eaa7a0
Guido Peirano <guidopeirano316@gmail.com>
2018-07-01 19:59:18 -0300

    fix2

--- api/routers/storiesRouter.py
+++ api/routers/storiesRouter.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
 from flask_httpauth import HTTPBasicAuth
 from flask import request
-from api.controllers.storiesController import getHomepageFeed, addNewStory, updateStory, removeStory, getSpecificUserStories
+from api.controllers.storiesController import getHomepageFeed, addNewStory, updateStory, removeStory, getSpecificUserStories, getLocationStories
 from SharedServerRequests.userLogin import *
 from databases.users import *
 from databases.loginedUsers import loginedUsers
